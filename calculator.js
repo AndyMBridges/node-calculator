7c72f25b8cbb4815584d55945de1ffa326ec5114
Andy Bridges <andymbridges@gmail.com>
2016-09-14 10:28:17 +0100

    Updated method

--- calculator.js
+++ calculator.js
@@ -7,36 +7,15 @@
 
 var readline = require('readline-sync');
 
-var Calculation = function(num1, num2, op) {
-  this.x = parseInt(num1);
-  this.y = parseInt(num2);
-  this.op = op;
+var num1 = readline.question("Please enter a number ");
+var num2 = readline.question("Please enter another number ");
+var operator = readline.question("Please enter an operator (+, - , / , * ) ");
 
-  this.result = function() {
-    var x = this.x;
-    var y = this.y;
-    var op = this.op;
+var operation = function(first_num, second_num, operator){
+  if(!isNaN(first_num) || !isNan(second_num)){
+    var result = eval(first_num + operator + second_num);
+    console.log(first_num + " " + operator + " " + second_num + " = " + result)
+  }
+};
 
-    if (op === "+") {
-      var result = x + y;
-    } else if (op === "-") {
-      var result = x - y;
-    } else if (op === "/") {
-      var result = x / y;
-    } else if (op === "*") {
-      var result = x * y;
-    } else {
-      var result = "Please enter +, - , / , * only!"
-    }
-
-    console.log("Result: " + result);
-  };
-}
-
-var num1 = readline.question("Please enter a number: ");
-var num2 = readline.question("Please enter another number: ");
-var op = readline.question("Please enter an operator (+, - , / , * ): ");
-
-var calc = new Calculation(num1, num2, op);
-
-calc.result();
+operation(num1, num2, operator);
